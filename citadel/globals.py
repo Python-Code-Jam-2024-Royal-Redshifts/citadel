7d89d12d2a10da92d700bae9fe2628a73ecb430d
Hunter Wittenborn <hunter@hunterwittenborn.com>
2024-07-22 20:37:46 -0500

    feat(app): decrease logging level

--- citadel/globals.py
+++ citadel/globals.py
@@ -8,7 +8,7 @@ _all_ = ["LOGGER"]
 
 # Logger
 logging.basicConfig(
-    level="NOTSET",
+    level=logging.INFO,
     format="%(message)s",
     datefmt="[%X]",
     handlers=[RichHandler(markup=True)],
